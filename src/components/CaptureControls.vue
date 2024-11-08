f6b8e2e92dbec4d95f64a35b5bb17dfd6e00a700
Jeff Simpson <jeff@jeff-simpson-digital.com>
2024-11-08 13:12:55 -0700

    Create jeff's Capture Controls for the top of the page

---
+++ src/components/CaptureControls.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="controls">
+    <input class="contorls__url" type="text" placeholder="Load a URL">
+    <button class="controls__url-button">GO</button>
+    <button class="contorls__capture">Select Catpure Area</button>
+  </div>
+</template>
+
+<style>
+  .controls {
+    padding: 1rem;
+    text-align: right;
+  }
+  input.controls__url{
+    margin-right: 0;
+  }
+  .controls__url-button {
+    margin-left: 0; 
+    margin-right: 1rem;
+  }
+</style>
