dc774306c56ba5007ebbba49cd183b76b91ee20b
Jeff Simpson <jeff@jeff-simpson-digital.com>
2024-11-08 13:12:31 -0700

    Jeff's updates to the App.vue file wraps content with an article tag and updates CSS

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <script setup>
+import CaptureControls from './components/CaptureControls.vue'
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import Test from './components/Test.vue'
+import LoadFromUrl from './components/LoadFromUrl.vue'
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <CaptureControls />
+  <article>
+    <header>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+      <LoadFromUrl />
+    </header>
 
-  <main>
-    <TheWelcome />
-    <Test />
-  </main>
+    <main>
+      <TheWelcome />
+      <Test />
+    </main>
+  </article>
 </template>
 
 <style scoped>
