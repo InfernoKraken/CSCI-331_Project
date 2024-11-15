5968361970bc88a79cbe0dd7ec76453d9a6f4828
Jeff Simpson <jeff@jeff-simpson-digital.com>
2024-11-15 13:10:16 -0700

    Update capture button wiht emit state

---
+++ src/components/ButtonCapture.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { defineEmits } from 'vue';
+
+  const emit = defineEmits(['toggle-visibility']);
+</script>
+
+<template>
+  <button @click="emit('toggle-visibility')">
+    Select Capture Aread
+  </button>
+</template>
+
+<style>
+
+</style>
